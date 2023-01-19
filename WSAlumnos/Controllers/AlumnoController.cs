0946a7c9a2a64185d634b917211813aad72fb34e
CanPe00 <candemperez@hotmail.com>
2023-01-19 17:57:19 -0300

    Agregar archivos de proyecto.

---
+++ WSAlumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WSAlumnos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+
+    }
+}
